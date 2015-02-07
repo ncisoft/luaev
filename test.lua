586d75ac32f7263320b11dfe7beef0b2f105b902
leeyg <ncisoft@hotmail.com>
2015-02-07 15:14:50 -0500

    start framework coding

--- test.lua
+++ test.lua
@@ -1,82 +1,38 @@
-local socket = require("socket")
-local logging = require("logging")
+local coscheduler = require("coscheduler")
+local comessages = require("comessage")
 local coroutine = require("coroutine")
-local debug = debug
-local table = table
-local table = require("table")
-local coroutine = require("coroutine")
-local logging = require("logging")
-local os = os
-local ipairs = ipairs
-local pairs = pairs
-local assert = assert
+local coutils = require("coutils")
 local print = print
-local type = type
-local tostring = tostring
-local debug = debug
-local arg = arg
-local base = _G
 
-local log = logging.new(function(self, level, message) 
-	local info = debug.getinfo(4)
-	local map = { info.short_src,":", info.name or "?", "():",info.currentline or "?"}
-	print(level, table.concat(map),message) return true end)
-log:setLevel (logging.DEBUG)
-local ss --= socket.tcp()
+local log = coutils.new_logger()
+local co_buyer = coroutine.create(function()
+	log:info("co_buyer was resume")
+	local me = comessages.new_mailbox("buyer")
 
-local function wrap_nil(map)
-	if (map == nil) then return "nil map" end
-	for k,v in pairs(map) do
-		if (v == nil) then map[k] = "nil" end
-	end
-end
+	coscheduler.detache()
+	log:info("222")
+	me:sendto("seller", "how much")
+	local sender_id, msg
+	log:info("333")
 
-local function run()
-
-	log:debug("--enter main()")
-	print(table.concat(debug.getinfo(1), " "))
-	assert(#arg == 1, "usage: lua test.lua port")
-	ss = socket.tcp()
-	assert(ss:bind("*",arg[1]))
-	assert(ss:listen(32))
-	--ss = assert(socket.bind("*", 7070))
-	assert(ss:setoption("reuseaddr", true))
-	local ip, port = ss:getsockname()
-	print("--enter main()...")
-	-- load listen coroutines
-	while true do
-		local r_set = {ss}
-		local w_set = {}
-		local ready_forread,msg
-		--table.insert(r_set, ss)
-
-		ready_forread, _,msg = socket.select(r_set, nil, 1)
-		for k,v in ipairs(ready_forread) do
-			if (v == ss) then
-				log:debug("accept event occur",msg)
-			end
-			log:debug({listen=v or "nil"})
-			local client,errmsg = v:accept()
-			log:debug({listen=ss or nil, client=client or "nil", error=errmsg or "nil"})
-			assert(client)
-			ss:close()
-			socket.sleep(1)
-			os.exit(0)
-			accepted_fd:settimeout(0)
-			print("--accept new connection\n")
-		end
-	end
-end
-local co_main = _G.coroutine.create(function(port)
-	port = port or 7070
+	sender_id,msg = me:receive()
+	print(msg)
 end)
 
+local co_seller = coroutine.create(function()
+	log:info("co_seller was resume")
+	local me = comessages.new_mailbox("seller")
+	local sender_id, msg
 
-	log:debug({"type", type(co_main)})
-if (run()) then
-	--
-else
-	ss:close()
-	print(debug.traceback ())
-	os.exit(0)
-end
+	log:debug(me)
+	coscheduler.detache()
+	sender_id,msg = me:receive()
+	me:sendto(sender_id, "99 dollars")
+	print(msg)
+end)
+
+coscheduler.join(co_buyer)
+coscheduler.join(co_seller)
+coscheduler.loop()
+
+log:fatal("exit");
--- uloop.lua
+++ uloop.lua
@@ -5,6 +5,7 @@ local coroutine = require("coroutine")
 local logging = require("logging")
 local term   = require 'term'
 local colors = term.colors -- or require 'term.colors'
+local strict = require("strict")
 local os = os
 local ipairs = ipairs
 local pairs = pairs
@@ -24,10 +25,33 @@ local log = logging.new(function(self, level, message)
 	local color_map = {}
 	color_map[ logging.DEBUG ] =  ""
 	color_map[ logging.INFO ] =  colors.yellow
-	color_map[ logging.WARN ] =  colors.red
+	color_map[ logging.WARN ] =  colors.cyanred
+	color_map[ logging.ERROR ] =  colors.red
+	--	if (false and level ~= logging.DEBUG) then
+	if (info.currentline ==163) then 
+		local i, flag = 2, true
+		while flag do
+			i=i+1	
+			print("....................."..i)
+			info = debug.getinfo(i)
+			if (info.currentline > 0) then
+				flag = false
+			elseif (info["source"] == "[C]") then 
+				info = debug.getinfo(4)
+				flag = false
+			else
+				for k,v in pairs(info) do
+					print(k.."="..tostring(v))
+				end
+			end
+		end
+	end
+	--info = debug.getinfo(4)
+
 	local color = color_map [ level ] or ""
 	local map = { info.short_src,":", info.name or "?", "():",info.currentline or "?"}
 	print(color, level, table.concat(map),message,colors.reset) return true end)
+
 log:setLevel (logging.DEBUG)
 
 local EV = {
@@ -76,10 +100,53 @@ local function join_listener(s)
 	log:debug({listen_socket_is=s})
 end
 
-local function join_read(_co, _socket)
+local function __join_read(_co, _socket)
 	if (s_maps[ socket ] == nil) then
 		s_maps[_socket] = {co=_co, socket=_socket, r_queue = {}}
 	end
+end
+
+local function __detache_read(_s)
+	if (s_maps[_s] ~= nil) then
+		if (s_maps[_s].w_queue == nil) then
+			s_maps[_s] = nil
+		else
+			s_maps[_s].r_queue = nil
+		end
+	end
+end
+local function __co_read(_co, _socket)
+	log:warn("......co_read was called")
+	log:warn({co=_co or "nil"})
+	__join_read(_co, _socket);
+	local data,msg = coroutine.yield()
+	log:warn("......co_read was resume.."..(data or "nil").." "..(msg or "nil msg"))
+	if (msg == "closed") then 
+		-- TODO: detach from reg_event
+		log:error({s=_socket, msg="the socket was closed"})
+		__detache_read(_socket)
+	end
+	return data,msg
+end
+
+local function __join_write(_co, _socket, data)
+	if (s_maps[_socket ] == nil) then
+		s_maps[_socket] = {co=_co, socket=_socket, w_queue = {}}
+	end
+	if (s_maps[_socket].w_queue == nil) then
+		s_maps[_socket].w_queue = {}
+	end
+	table.insert(s_maps[_socket].w_queue, data)
+	log:debug({s=s_maps[_socket]})
+end
+
+local function __co_write(_co, fd, data)
+	log:info({ev="......co_write was called", data=data or "nil"})
+	data = data or "+hello\n"
+	__join_write(_co, fd, data)
+	local rc = coroutine.yield()
+	log:warn("......co_write was resumed"..rc)
+	return rc
 end
 
 local function co_accept(_co, listen_socket)
@@ -91,13 +158,13 @@ local function co_accept(_co, listen_socket)
 			local co = coroutine.running()
 			log:warn("child.fork coroutine was resumed")
 			while 1 do
-				local data	= __co_read(co, fd)
-				if(data == nil) then
-					log:debug("client is dead\n")
-					assert(data)
+				local data,err	= __co_read(co, fd)
+				if(err == "closed") then
+					log:error("client is dead")
+					assert(false)
 				end
 				log:debug("receive..."..data.."\n")
-				co_write(co, fd, "+"..data)
+				__co_write(co, fd, "+"..data)
 			end
 		end)
 		log:debug("accept a child")
@@ -105,26 +172,6 @@ local function co_accept(_co, listen_socket)
 	end
 end
 
-local function __co_read(_co, _socket)
-	log:warn("......co_read was called")
-	print("lalllaaa")
-	join_read(co, _socket);
-	data = coroutine.yield()
-	return data
-end
-
-local function co_write(_co, fd, data)
-	join_write(_co, _socket, data)
-	local rc = coroutine.yield(_co)
-	return rc
-end
-
-local function join_write(_co, _socket, data)
-	if (s_maps[_socket ] == nil) then
-		s_maps[_socket] = {co=_co, socket=_socket, w_queue = {}}
-	end
-	table.insert(s_maps[_socket].w_queue, data)
-end
 
 local function join_close(_co, _socket)
 
@@ -135,6 +182,7 @@ local function schedule(init_co,port)
 	log:debug("enter main loop, type(init_co)="..type(init_co))
 	coroutine.resume(init_co, port)
 	log:debug("after resume main coroutine")
+	local n=0
 	while 1 do
 		local r_set = {}
 		local w_set = {}
@@ -143,11 +191,13 @@ local function schedule(init_co,port)
 			if (v.is_listen == true or v.r_queue ~= nil) then
 				table.insert(r_set, v.socket)
 			end
-			if (v.w_queue ~= nil) then
+			if (v.w_queue ~= nil and #v.w_queue > 0) then
 				table.insert(w_set, v.socket)
 			end
 		end
-		log:debug({r_set_size = #r_set, w_set_size = #w_set, rset=r_set[1]})
+		n = n+1
+		assert(n < 18)
+		log:info({r_set_size = #r_set, w_set_size = #w_set, rset=r_set[1], count=n})
 		assert(#r_set > 0 or #w_set > 0)
 		local readable, writeable, msg = socket.select(r_set, w_set, nil)
 		for _, s in ipairs(readable) do
@@ -162,30 +212,44 @@ local function schedule(init_co,port)
 				coroutine.resume(o.co, fd)
 			end
 			if (o.r_queue ~= nil) then
-				local data,errmsg = s.receive(MTU_SIZE)
+				log:debug({s=s or "nil"})
+				local data,errmsg = s:receive(7)
 				if (data == nil) then
 					log:debug(errmsg.."--read--nil")
+					if (errmsg == "closed") then
+						log:error({socket_is_closed=true, will_resume_read_co=o, co_status=coroutine.status(o.co) or "nil"})
+						coroutine.resume(o.co, nil, "closed")
+					elseif (errmsg == "timeout") then
+						-- will continue to read
+					end
+				else
+					log:debug({will_resume_read_co=o})
+					table.insert(o.r_queue, data)
+					log:debug(o)
+					coroutine.resume(o.co, data)
+					table.remove(o.r_queue)
 				end
-				table.insert(v.r_queue, data)
-				coroutine.resume(v.co, data)
 			end
 		end
-		for i, s in ipairs(writeable) do
+		for _, s in ipairs(writeable) do
 			local v = s_maps[s]
-			if (v.w_queue ~= nil) then
-				local rc,errmsg,pos = s.send(v.w_queue[1])
+			if (v.w_queue ~= nil and #v.w_queue > 0) then
+				log:debug({will_resume_write_co=v})
+				local rc,errmsg,pos = s:send(v.w_queue[1])
 				if (rc == nil) then
 					log:debug(errmsg.."--write--nil")
-				coroutine.resume(v.co, false)
+					coroutine.resume(v.co, false)
+				else
+					log:debug({write_rc=rc})
+					table.remove(v.w_queue, 1)
+					coroutine.resume(v.co, rc)
 				end
-				table.remove(v.w_queue, 1)
-				coroutine.resume(v.co, true)
 			end
 		end
 	end
 end
 
-co_main = base.coroutine.create(function(port)
+local co_main = base.coroutine.create(function(port)
 	port = port or 7070
 	log:debug("--enter main(),will listen on port:"..port)
 	local s-- = socket.tcp()
